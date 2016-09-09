dbeb921639adeb39f34ce667e3b4d84851c9fdcf
mwatanabe <watanabe.seman@gmail.com>
2016-09-09 10:51:52 +0900

    first implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # simple-logging-server
 A simple server implementation for logging http requests.
+
+# Usage
+```
+node server.js
+```
+
+# License
+MIT
---
+++ server.js
@@ -0,0 +1,23 @@
+http = require('http');
+
+server = http.createServer(function(req, res) {
+    console.log('Url: ' + req.url);
+    console.log('Method: ' + req.method);
+    console.log('Headers: ');
+    console.dir(req.headers);
+
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+    });
+    req.on('end', function () {
+        console.log('Body: ' + body + '\n');
+        res.end();
+    });
+});
+
+port = 3000;
+host = '127.0.0.1';
+server.listen(port, host);
+console.log('Listen on http://' + host + ':' + port);
+
